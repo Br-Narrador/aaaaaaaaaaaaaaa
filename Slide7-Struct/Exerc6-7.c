1999fcfeffc8f7f72bc1effed2eb4b757b0c9a88
Br-Narrador <50932210+Br-Narrador@users.noreply.github.com>
2025-09-20 15:46:41 -0300

    Update Exerc6-7.c

--- Slide7-Struct/Exerc6-7.c
+++ Slide7-Struct/Exerc6-7.c
@@ -1,24 +1,25 @@
 #include<stdio.h>
 struct personagem{
-  char[10] nome;
+  char nome;
   int eliminacoes;
   int mortes;
   int vit;
   int def;
-}
-void formatar(struct persona)
+};
+void formatar(struct personagem persona)
 {
-  printf("O jogador: %s\n", persona.nome);
+  printf("\nO jogador: %c\n", persona.nome);
   printf("Possui uma taxa de eliminacoes por morte igual a: %d/%d\n", persona.eliminacoes, persona.mortes);
   printf("Alem disso, ele possui %d vitorias e %d derrotas\n", persona.vit, persona.def);
 }
-void preenche(struct* pesona){
+void preenche(struct personagem* persona){
   printf("Nome do jogador: ");
-  scanf("%s", persona.nome);
+  scanf("%c", &persona -> nome);
   printf("Quantas eliminacoes? E quantas mortes?");
-  scanf("%d %d", persona.eliminaceos, persona.mortes);
+  scanf("%d %d", &persona -> eliminacoes, 
+  &persona-> mortes);
   printf("Quantas vitorias? E quantas derrotas?");
-  scanf("%d %d", persona.vit, persona.def);
+  scanf("%d %d", &persona -> vit, &persona -> def);
 }
 int main(){
   struct personagem player;
