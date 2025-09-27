e45105e8bac46f6692f2a695f3d636dfbb21c6df
Br-Narrador <50932210+Br-Narrador@users.noreply.github.com>
2025-09-27 20:17:47 -0300

    Create exerc5.c

---
+++ Slide7-Union/exerc5.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdbool.h>
+#include<string.h>
+typedef struct Anfibio{
+  int numeroPatas;
+  int espessuraPele;
+  bool calda;
+} Anfibio;
+typedef struct Aves{
+  
+}Aves;
+typedef struct Mamiferos{
+
+}Mamiferos;
+typedef struct Repteis{
+
+}Repteis;
